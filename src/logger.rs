b0fe02f5aa3f5b3ada542e3418234d8349ca66a1
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2024-09-02 15:52:56 +1200

    Added log folder

--- src/logger.rs
+++ src/logger.rs
@@ -1,15 +1,30 @@
 use simplelog::*;
-use std::fs::File;
+use std::fs::{self, File};
+use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub fn initialize() {
+    // Create the "logs" directory if it doesn't exist
+    let log_dir = "logs";
+    if !Path::new(log_dir).exists() {
+        fs::create_dir(log_dir).unwrap();
+    }
+
+    // Generate a unique filename using the current timestamp
+    let start_time = SystemTime::now();
+    let since_epoch = start_time.duration_since(UNIX_EPOCH).unwrap();
+    let timestamp = since_epoch.as_secs();
+    let log_file_path = format!("{}/app_{}.log", log_dir, timestamp);
+
     CombinedLogger::init(vec![
-        // Write logs to a file
+        // Write logs to a unique file
         WriteLogger::new(
             LevelFilter::Info, // Set the logging level
             Config::default(),
-            File::create("app.log").unwrap(), // The log file
+            File::create(&log_file_path).unwrap(), // The log file
         ),
     ])
     .unwrap();
 }
+
 pub use log::{debug, error, info};
